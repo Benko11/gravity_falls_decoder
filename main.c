b6008ebbbd7bcb66f7fd4cb5f870a1c2e83b9e69
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2022-02-02 17:31:03 +0100

    Bug fixes

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 void welcome(void);
 void help(int error);
 char menu(void);
+void draw_bill_easter_egg(void);
 
 int main(int argc, char const *argv[]) {
     if (argc == 2 && strcmp(argv[1], "help") == 0) {
@@ -212,12 +213,17 @@ int main(int argc, char const *argv[]) {
             printf("Text: ");
             fscanf(stdin, " %s", text);
 
-            char *result = (char *) malloc(512 * 8 * sizeof(char));
-            result = binary_cipher(text);
+            char *upper_text = strtouppercase(text);
+            if (strcmp(upper_text, "TRIANGULUM,ENTANGULUM,METEFORIS_DOMINUS_VENTIUM.METEFORIS_VENETISARIUM!") == 0){
+                draw_bill_easter_egg();
+            } else {
+                char *result = (char *) malloc(512 * 8 * sizeof(char));
+                result = binary_cipher(text);
 
-            printf("%s\n", result);
+                printf("%s\n", result);
+                free(result);
 
-            free(result);
+            }
         }
             break;
         case '6': {
@@ -294,4 +300,103 @@ void help(int error) {
     fprintf(error == 1 ? stderr : stdout, "Binary decipher: 6 | bind\n");
     fprintf(error == 1 ? stderr : stdout, "Atbash: 7 | atbash\n\n");
     fprintf(error == 1 ? stderr : stdout, "usage: ./main.out - enters the CLI\n");
+}
+
+void draw_bill_easter_egg(void) {
+    puts("\n");
+
+puts("                                                                      hmddhhyyyhh:                                                                    ");
+puts("                                                                      dMMMMMMMMMM/                                                                    ");
+puts("                                                                      hMMMMMMMMMM/                                                                    ");
+puts("                                                                      yMMMMMMMMMM-                                                                    ");
+puts("                                                                      sMMMMMMMMMM.                                                                    ");
+puts("                                                                      sMMMMMMMMMM                                                                     ");
+puts("                                                                      +MMMMMMMMMN                                                                     ");
+puts("                                                                      /MMMMMMMMMN                                                                     ");
+puts("                                                                      -MMMMMMMMMm                                                                     ");
+puts("                                                                      `MMMMMMMMMm                                                                     ");
+puts("                                                                       NMMMMMMMMm                                                                     ");
+puts("                                                                       dMMMMMMMMh                                                                     ");
+puts("                                                                ///////dMMMMMMMMd+++++/.                                                              ");
+puts("                                                               .MMMMMMMMMMMMMMMMMMMMMMM+                                                              ");
+puts("                                                                .-:::::----------------`                                                              ");
+    printf("\033[1;33m");                                                                                                       
+
+puts("                                                                         `smm:                                                                        ");
+puts("                                                                        `yMymN/                                                                       ");
+puts("                                                                       .hNs-:mN/                                                                      ");
+puts("                                                                      -dNo---/mN:                                                                     ");
+puts("                                                                     :mN+-----/mN:                                                                    ");
+puts("                                                                    /Nm/-------/mN+                                                                   ");
+puts("                                                                   oMd:---------:dMs                                                                  ");
+puts("                                                                  sMh-------------yMy`                                                                ");
+puts("                                                                 yMy---------------sMd.                                                               ");
+puts("                                                               `hMy-----------------+NN-                                                              ");
+puts("                                                              `dMs------+------/-----/mN/                                                             ");
+puts("                       -/                                    .dNo-/-----d------y-----/:mN+                                    +:                      ");
+puts("                       dM:                                  .mN+--oo----d/----/s----oo-:dMo                                   NN                      ");
+puts("                  `oo- +Mm:    :do                         -mN+----os+oydhyyysdy+/:s+---:hMs`                        .ho`    oMd  :h/                 ");
+puts("                  `hMNhsdMNh+/oNN/                        :Nm/---+yhho/-...+-..-:+sho+:---yMy`                       `smds+/hMNosdNh-                 ");
+puts("                   `:+shNMMMMmds-                        /Nm/-:ohy/.      .No      `-/ss+--yMh`                        .+yNMMMMmyo-`                  ");
+puts("                  `yyyhdmmmMM:`                         +Nd:-+dy-         +Mm          .oy+-sMh.                          yMMNNmysyh`                 ");
+puts("                  `/ooo/:.sMN`                         oMd:-sd:           oMM.           .ss-sNd.                         /MM::+oso:                  ");
+puts("                          yMm                        `sMh--dm.            oMM-             +s-oNm-                        .MM.                        ");
+puts("                          sMm                       `yMy---dy`            +MM:             -h--+Nm-                       .MM`                        ");
+puts("                          +Mm                      `dMs-----hh.           +MM-            /m/---/NN:                      .MN                         ");
+puts("                          :MN                     .mMo-------+do`         +MN`          -hd:-----:mM/                     /Mh                         ");
+puts("                          `MM.                   -NN/----------+hy:`      -My        `/hd+--------:dM+                    hM+                         ");
+puts("                           dMs                `-+Nm/-------------:shy+:.   /.    .:oyhs/------------dMo.                 :Mm`                         ");
+puts("                           -NMh/-`````..-:+oymNNMm:----------------sy+sydhyyyyhhhysm+----------------hMNmhso/:--.`     .+NN-                          ");
+puts("                            -yNMNNNNNNNNNNmdhsmMd:----------------y+----h+::::oo---:y/----------------hMmsyhdmNNNNmddmmNNy.                           ");
+puts("                              .:+ooo++/:-..``sMh-----------------:+-----d-----:y-----/-----------------yMy```.-:/osyyys+.                             ");
+puts("                                           `yMy-------------------------+------/------------------------yMh`                                          ");
+puts("                                          .hNs-----------------------------------------------------------sNd.                                         ");
+puts("                                         .dNs::::::/://://////////++/+//+++o++++++++++++++++++++++++o+++//yNm-                                        ");
+puts("                                        -mN+-------o--------------sho/-----o-----:ohh---------------o------/mN/                                       ");
+puts("                                       :Nm/--------s--------------sMMNds/:-o--:ohNMMN---------------o-------/mN+                                      ");
+puts("                                      /Nm/:::::::::s:::--------:::yMMMMMMmhyydMMMMMMN///+//+++++++++y++///////mMo                                     ");
+puts("                                     +Md/::::::::::::://///////h//yMMMMMNds+ohNMMMMMM:::o::::::::::::::::::::::hMy`                                   ");
+puts("                                    sMh------------------------y--yMMds+-------/shNMM:-:o-----------------------sMh`                                  ");
+puts("                                  `hMy-------------------------y--+o:--------------/o--/s------------------------oMd.                                 ");
+puts("                                 .dMy::////////////////////////y///////////////////////+y////:::::::::::::::::::::sMm.                             ");   
+puts("                                .mNo:::------------h:::::::::::::::::::::::o/::::::::::::::::://////o///////////:::oNm-                            ");   
+puts("                               :mN+----------------h-----------------------//-----------------------+---------------+Nm:                        ");      
+puts("                              oNN/-----------------y-----------------------//-----------------------o----------------+NN/                    ");         
+puts("                            `sMMdyyyhhhhhhhhhhyyyyymyyyyyyyyyyyyyyyyhhhhhhhddhhhhhhhhhhhhyyyyyysssooho++++//////////++yMNo                ");            
+puts("                            `sysoo+++++++++++++++++++oooooooomMMMy+++++///////////////hMMmooooossyyyhhhhdddddddddddddhhys+             ");               
+puts("                                                            sMMMy`                    `dMM+               ``````````                ");                  
+puts("                                                           :MMMd`                      .NMN-                                     ");                     
+puts("                                                          `mMMN.                        /MMd`                                 ");                        
+puts("                                                          yMMM/                          sMMo                              ");                           
+puts("                                                         :MMMh                           `mMM-                          ");                              
+puts("                                                         dMMM-                            /MMh                       ");                                 
+puts("                                                        `MMMm                              mMM.                   ");                                    
+puts("                                                        -MMMy         `+mm:                sMM+                ");                                       
+puts("                                                        `NMMm        /mMMMy                +MMs             ");                                          
+puts("                                                         +MMMy:...:omMMMMMh  -s+.          +MMs          ");                                             
+puts("                                                          +mMMNNmNNNd/dMMMs  yMMNy-       .mMM/       ");                                                
+puts("                                                           .:oyhhy+-` sMMM+  sMMMMNy:.``./dMMy`    ");                                                   
+puts("                                                                      oMMM.  oMMMyyNMmddmMMmo`  ");                                                      
+puts("                                                                      /MMh   /MMM+ .+yhddy/.");                                                         
+puts("                                                                       +s.   -MMM/");                                                                    
+puts("                                                                              mMN.");                                                                    
+puts("                                                                              .+-");
+printf("\033[1;34m");
+puts("\nAs your eyes are glowing blue");
+puts("You find you wish not to see the truth");
+puts("The omnipotent being appears in front of your eyes");
+puts("You are learning that he is most divinely wise\n\n");
+
+printf("\033[1;31m");
+puts("HA-HA-HA-HA-HA-HA-HA!");
+puts("NAME'S BILL CIPHER! AND I TAKE IT YOU'RE SOME KIND OF VENTRILOQUIST DUMMY?");
+puts("HA-HA!");
+puts("I'M JUST KIDDING, I KNOW WHO YOU ARE!\n");
+puts("WELL, WELL, WELL, WELL, WELL, WELL, WELL, WELL, WELL ... !");
+puts("AREN'T YOU A SIGHT FOR SORE EYES?\n");
+puts("HA-HA-HA-HA-HA-HA-HA!");
+puts("PHYSICAL FORM DON'T MIND IF I DO!");
+puts("FOR ONE TRILLION YEARS I'VE BEEN TRAPPED IN MY OWN DECAYING DIMENSION, WAITING FOR A NEW UNIVERSE TO CALL MY OWN!");
+puts("NAME'S BILL! BUT YOU CAN CALL ME YOUR LORD AND MASTER FOR ALL ETERNITY!");
+puts("THIS PARTY NEVER STOPS! TIME IS DEAD AND MEANING HAS NO MEANING! EXISTENCE IS UPSIDE DOWN AND I REIGN SUPREME!");
 }
